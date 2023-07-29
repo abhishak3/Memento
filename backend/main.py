b382370d4b817e8622a7a9acccc43432a49cff83
abhishak3 <abhishek.pandey.3237@gmail.com>
2023-07-29 12:49:08 +0530

    updated .gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "https://memento-iota.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/')
-def home():
-    return {"success": True}
+async def home():
+    return {"success": True, "error": None}
